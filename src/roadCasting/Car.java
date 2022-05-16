6a081307329503d3b65c261ef12e8bffd872667b
sunsetkk <ssunset.kr@gmail.com>
2022-05-16 22:10:05 +0900

    ADD casting

---
+++ src/roadCasting/Car.java
@@ -0,0 +1,22 @@
+package roadCasting;
+
+public class Car {
+	String brand;
+	String color;
+	int price;
+	
+	public Car() {}
+	
+	public Car(String brand, String color, int price) {
+		this.brand = brand;
+		this.color = color;
+		this.price = price;
+	}
+	
+	void engineStart() {
+		System.out.println(brand + " ¿­¼è·Î ½Ãµ¿ ÄÑ±â");
+	}
+	void engineStop() {
+		System.out.println(brand + " ¿­¼è·Î ½Ãµ¿ ²ô±â");
+	}
+}
