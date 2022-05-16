03978110fd2118bbaf1721d1c2854e67f8b9206a
sunsetkk <ssunset.kr@gmail.com>
2022-05-16 22:09:25 +0900

    MODIFY Ground.java

--- src/zoo/Ground.java
+++ src/zoo/Ground.java
@@ -5,6 +5,11 @@ public class Ground {
 		//동물 세마리 만들어서 울게하기
 		//각 동물은 울음소리 다르게
 		//각 동물마다 특징적인 행동들 하나씩 하기
+		
+//		Cat cat = new Cat("키티", "여", 8);
+//		Dog dog = new Dog("개리", "남", 7);
+//		Meerkat meer = new Meerkat("꾸릉", "여", 5);
+		
 		Animal[] zoo = {
 				new Cat("키티", "여", 8),
 				new Dog("개리", "남", 7),
