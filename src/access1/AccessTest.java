b2d93f54e444596cf4edd78d8d5aa252fec9845d
sunsetkk <ssunset.kr@gmail.com>
2022-05-21 18:11:19 +0900

    ADD access

---
+++ src/access1/AccessTest.java
@@ -0,0 +1,20 @@
+package access1;
+
+public class AccessTest {
+	int data1 = 10;
+	public int data2 = 20;
+	private int data3 = 30;
+	protected int data4 = 40;
+	
+	//Alt + Shift + S > R : getter, setter 메서드 만들기
+	//getter 메서드
+	public int getData3() {
+		return data3;
+	}
+	//setter 메서드
+	public void setData3(int data3) {
+		this.data3 = data3;
+	}
+	
+	
+}
---
+++ src/access2/AccessMain2.java
@@ -0,0 +1,23 @@
+package access2;
+
+//다른 패키지에 있는 클래스를 이용하려면 import가 필요하다.
+import access1.AccessTest;
+
+public class AccessMain2 extends AccessTest{
+	public static void main(String[] args) {
+		AccessTest obj = new AccessTest();
+//		System.out.println(obj.data2);
+		
+//								obj.data3
+		System.out.println(obj.getData3());
+		
+//		obj.data3 = 300;
+		obj.setData3(300);
+	}
+	
+	void f() {
+//		System.out.println(data1); //(default)
+		System.out.println(data4); // protected
+		
+	}
+}
