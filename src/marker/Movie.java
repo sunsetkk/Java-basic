8ec082dc17a1f4b893a8b811653fa987fe215ec5
sunsetkk <ssunset.kr@gmail.com>
2022-05-23 17:41:00 +0900

    ADD interface task

---
+++ src/marker/Movie.java
@@ -0,0 +1,13 @@
+package marker;
+
+public class Movie {
+	String name;
+	
+	public Movie() {}
+	public Movie(String name) {
+		this.name = name;
+	}
+	void show() {
+		System.out.println(name + " »ó¿µÁß");
+	}
+}
