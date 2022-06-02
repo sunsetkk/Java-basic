8ad8bc9a5da574acd9a884694dca3df5caf928cd
sunsetkk <ssunset.kr@gmail.com>
2022-06-02 15:31:46 +0900

    MODIFY exception

--- src/exception/ThrowsTest.java
+++ src/exception/ThrowsTest.java
@@ -4,15 +4,32 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class ThrowsTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
+		div(10,3);//	10/3
+		
 		try {
-			f();
-		} catch (FileNotFoundException e) {
+			div(10,1);
+		} catch (ArithmeticException ae) {
+			
+		}
+		
+		try {
+			div(20,1);
+		} catch (FileNotFoundException fnfe) {
+			System.out.println("¿Í¿Í");
 		}
 	}
-	static void f() throws FileNotFoundException {
+	
+	static int div(int num1, int num2) throws FileNotFoundException {
 //		Alt + Shift + Z > ¡é > enter : try~catch¹®
-		new FileReader("");
+		if(num2 == 0) {
+			throw new ArithmeticException();
+		}
+		if(num2 == 1) {
+			throw new FileNotFoundException();
+		}
+		
+		return num1/num2;
 	}
 	
 }
